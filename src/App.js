770fb51874fde3b36673d8df40ad07950dc01479
kristinkaklyap <kristinka.klyap@gmail.com>
2022-11-03 21:06:54 +0100

    update README, remove trashes from App.js component

--- src/App.js
+++ src/App.js
@@ -10,12 +10,7 @@ function App() {
     }
   return (
     <div className={`App ${theme}`}>
-      <Title bordered={true} alignment='to_left' headingLevel={1} label={'My UI Kit'}/>
-      <Title bordered={true} alignment='to_center'  headingLevel={2} label={'My UI Kit'}/>
-      <Title bordered={true} alignment='to_right'  headingLevel={3} label={'My UI Kit'}/>
-      <Title bordered={true} headingLevel={4} label={'My UI Kit'}/>
-      <Title headingLevel={5} label={'My UI Kit'}/>
-      <Title headingLevel={6} label={'My UI Kit'}/>
+      <Title bordered={true} alignment='to_center'  headingLevel={1} label={'My UI Kit'}/>
         <p>Lorem ipsum dolor sit amet</p>
         <button onClick={changeThemeHandler}>Switch theme</button>
     </div>
